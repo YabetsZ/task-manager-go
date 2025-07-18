5c5f43c00281543a4752978053ac187b6cfb4f61
YabetsZ <yabetszekaryas07@gmail.com>
2025-07-19 02:33:54 +0300

    feat(task_service): Integrate MongoDb for persistent storage.
    - All services related to tasks have been modified so that they can be used with mongodb.
    - some functions' signature have been changed so that they can accommodate the new changes.

--- data/task_service.go
+++ data/task_service.go
@@ -1,75 +1,138 @@
 package data
 
 import (
+	"context"
 	"fmt"
 	"task-manager/models"
+	"time"
 
-	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type TaskService struct {
-	tasks map[string]*models.Task
+	collection *mongo.Collection
 }
 
-func NewTaskService() *TaskService {
+func NewTaskService(collection *mongo.Collection) *TaskService {
 	return &TaskService{
-		tasks: make(map[string]*models.Task),
+		collection,
 	}
 }
 
-func (ts *TaskService) CreateTask(task models.Task) *models.Task {
+func (ts *TaskService) CreateTask(task models.Task) (*models.Task, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	if task.Status == "" {
 		task.Status = models.StatusPending
 	}
 
-	task.ID = uuid.NewString()
+	task.ID = primitive.NewObjectID()
 
-	ts.tasks[task.ID] = &task
-
-	return &task
-}
-
-func (ts *TaskService) GetTasks() []*models.Task {
-	tasks := make([]*models.Task, 0, len(ts.tasks))
-	for _, task := range ts.tasks {
-		tasks = append(tasks, task)
+	_, err := ts.collection.InsertOne(ctx, task)
+	if err != nil {
+		return nil, err
 	}
 
-	return tasks
+	return &task, nil
+}
+
+func (ts *TaskService) GetTasks() ([]*models.Task, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	tasks := make([]*models.Task, 0)
+
+	cursor, err := ts.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	err = cursor.All(ctx, &tasks)
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
 }
 
 func (ts *TaskService) GetTaskByID(id string) (*models.Task, error) {
-	task, exist := ts.tasks[id]
-	if !exist {
-		return nil, fmt.Errorf("couldn't find a taks with id %s", id)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid id format: %w", err)
 	}
-	return task, nil
+
+	var task models.Task
+
+	err = ts.collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&task)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("couldn't find a taks with id %s", id)
+		}
+		return nil, err
+	}
+
+	return &task, nil
 }
 
 func (ts *TaskService) UpdateTask(id string, updatedTask models.Task) (*models.Task, error) {
-	if _, exist := ts.tasks[id]; !exist {
-		return nil, fmt.Errorf("couldn't find a taks with id %s", id)
-	}
-	if updatedTask.Description != "" {
-		ts.tasks[id].Description = updatedTask.Description
-	}
-	if updatedTask.DueDate.IsZero() {
-		ts.tasks[id].DueDate = updatedTask.DueDate
-	}
-	if updatedTask.Status != "" {
-		ts.tasks[id].Status = updatedTask.Status
-	}
-	if updatedTask.Title != "" {
-		ts.tasks[id].Title = updatedTask.Title
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid id format: %w", err)
 	}
 
-	return ts.tasks[id], nil
+	updateFields := bson.M{}
+	if updatedTask.Description != "" {
+		updateFields["description"] = updatedTask.Description
+	}
+	if !updatedTask.DueDate.IsZero() {
+		updateFields["due_date"] = updatedTask.DueDate
+	}
+	if updatedTask.Status != "" {
+		updateFields["status"] = updatedTask.Status
+	}
+	if updatedTask.Title != "" {
+		updateFields["title"] = updatedTask.Title
+	}
+	update := bson.M{
+		"$set": updateFields,
+	}
+
+	res, err := ts.collection.UpdateByID(ctx, objID, update)
+	if err != nil {
+		return nil, err
+	}
+	if res.MatchedCount == 0 {
+		return nil, fmt.Errorf("couldn't find a taks with id %s", id)
+	}
+
+	return ts.GetTaskByID(id)
 }
 
 func (ts *TaskService) DeleteTask(id string) error {
-	if _, err := ts.GetTaskByID(id); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return fmt.Errorf("invalid id format: %w", err)
+	}
+
+	res, err := ts.collection.DeleteOne(ctx, bson.M{"_id": objID})
+	if err != nil {
 		return err
 	}
-	delete(ts.tasks, id)
+	if res.DeletedCount == 0 {
+		return fmt.Errorf("couldn't find a taks with id %s", id)
+	}
+
 	return nil
 }

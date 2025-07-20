8d85af32acadca2cf22f444a920710d0e2497c64
YabetsZ <yabetszekaryas07@gmail.com>
2025-07-20 11:10:34 +0300

    feat: Add `User` new struct

--- models/user.go
+++ models/user.go
@@ -1,1 +1,15 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+const (
+	RoleAdmin = "admin"
+	RoleUser  = "user"
+)
+
+type User struct {
+	ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Username     string             `json:"username" bson:"username"`
+	PasswordHash string             `json:"-" bson:"password_hash"`
+	Role         string             `json:"role" bson:"role"`
+}
